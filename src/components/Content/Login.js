f7564296e18e58bb87e66cf2c63eec8ae181199f
joshborup <joshborup@gmail.com>
2018-10-15 02:41:43 -0700

    updated login component

--- src/components/Content/Login.js
+++ src/components/Content/Login.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+    constructor(){
+        super()
+        this.state = {
+            name: ''
+        }
+    }
+
+
     render() {
         return (
             <div>
-                login
+                <h1>Login!</h1>
+                <input value={this.state.name} onChange={(e) => this.setState({name: e.target.value})}/>
+                <button onClick={() => this.props.login(this.state.name)}>Submit</button>
             </div>
         );
     }
